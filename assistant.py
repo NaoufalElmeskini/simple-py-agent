e4a07b04da90597406938f850e6bf65a1de55771
tintin <tintin@Host-002.lan>
2024-11-12 11:53:08 +0100

    etape 2 : ajouter chat avec mémoire

--- assistant.py
+++ assistant.py
@@ -1,8 +1,14 @@
 import ollama
 
-localModel = 'phi3:mini'
-output = ollama.generate(model=localModel, prompt='capitale de la France ?')
-response = output['response'];
 
-print(response)
+localModel = 'phi3:mini'
+conversation = []
+
+while True:
+    prompt = input('USER: \n')
+    conversation.append({'role': 'user', 'content': prompt})
+    output = ollama.chat(model=localModel, messages=conversation)
+    response = output['message']['content']
+    print(response)
+    conversation.append({'role': 'assistant', 'content': response})
 
